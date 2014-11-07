3d581290e1f0f835a4f0a0a831718ea71fe25f68
Dan Kee <dan@dankee.com>
2014-11-06 21:31:00 -0600

    Visualize bookmarks as nested list.

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html" }
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2356"] ]
+                 [org.clojure/clojurescript "0.0-2356"]
+                 [prismatic/dommy "1.0.0"] ]
   :plugins [[lein-cljsbuild "1.0.3"]]
   :source-paths []
 
--- src/core.cljs
+++ src/core.cljs
@@ -1,4 +1,31 @@
-(ns dkbookmarks.core)
+(ns dkbookmarks.core
+  (:require [dommy.core :as dommy :refer-macros [sel1]]) )
 
-(.log js/console "hello bookmarks")
+(enable-console-print!)
+
+(defn print-object [object]
+  (.log js/console object) )
+
+(defn with-root-node [callback]
+  (.getTree js/chrome.bookmarks (fn [results] (callback (first results)))) )
+
+(defn append-element [parent element]
+  (dommy/append! parent element)
+  element )
+
+(defn new-list []
+  (dommy/create-element :ul) )
+
+(defn new-list-item [text]
+  (dommy/set-text! (dommy/create-element :ul) text) )
+
+(defn print-node
+  ([node] (print-node node (append-element (sel1 :#root) (new-list))))
+  ([node parent-ul]
+     (append-element parent-ul (new-list-item (.-title node)))
+     (let [child-ul (append-element parent-ul (new-list))]
+       (doseq [child (.-children node)]
+         (print-node child child-ul) ) ) ) )
+
+(with-root-node print-node)
 
