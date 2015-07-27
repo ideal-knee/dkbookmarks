bfad4c60c5696df12c902efe5ff226eebdf4c270
Dan Kee <dan@dankee.com>
2015-07-27 00:36:11 -0500

    Add cider ClojureScript support.

--- project.clj
+++ project.clj
@@ -4,11 +4,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html" }
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2356"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "0.0-3308"]
                  [prismatic/dommy "1.0.0"] ]
-  :plugins [[lein-cljsbuild "1.0.3"]]
+  :plugins [[lein-cljsbuild "1.0.3"]
+            [cider/cider-nrepl "0.9.1"] ]
   :source-paths []
+
+  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.2.1"]
+                                  [org.clojure/tools.nrepl "0.2.10"]]
+                                    :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
 
   :cljsbuild {:builds [{:source-paths ["src"]
                         :compiler {:output-to "gen/script.js"
